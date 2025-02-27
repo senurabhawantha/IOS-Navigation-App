00ea515d6414435719b1d91e2955a69fca20609c
senurabhawantha <senurabawantha@gmail.com>
2025-02-27 18:15:15 +0530

    Initial Commit

---
+++ IOS-Navigation-App/IOS_Navigation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Navigation_AppApp.swift
+//  IOS-Navigation-App
+//
+//  Created by ITEDP on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Navigation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
